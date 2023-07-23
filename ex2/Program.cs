8ca6a0808cf48d0ccbff82bb58f8d082ce34c653
Alexey <kalnitskiy2018@mail.ru>
2023-07-23 12:22:32 +0500

    ex2

---
+++ ex2/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] arr = new int[10];
+int sum = 0;
+for(int i = 0; i < arr.Length; i++){
+    arr[i] = new Random().Next(-10, 11);
+    System.Console.Write("["+arr[i]+"]"+" ");
+}
+System.Console.WriteLine();
+for(int i = 1; i < arr.Length; i+=2){
+    sum = sum + arr[i];
+}
+System.Console.WriteLine(sum);
