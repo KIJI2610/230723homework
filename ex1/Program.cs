b659eff591faf0cca922d8d314bd9375e7fb5181
Alexey <kalnitskiy2018@mail.ru>
2023-07-23 12:08:55 +0500

    ex1

---
+++ ex1/Program.cs
@@ -0,0 +1,17 @@
+﻿System.Console.WriteLine("Весь массив: ");
+int[] arr = new int[10];
+int count = 0;
+for(int i = 0; i < arr.Length; i++){
+    arr[i] = new Random().Next(100, 1000);
+    System.Console.Write("["+arr[i]+"]"+" ");
+}
+System.Console.WriteLine();
+System.Console.WriteLine("Четные значения:");
+for(int i = 0; i < arr.Length; i++){
+    if(arr[i]%2 == 0){
+        System.Console.Write("["+arr[i]+"]"+" ");
+        count++;
+    }
+}
+System.Console.WriteLine();
+System.Console.WriteLine("Количество четных значений: "+count);
