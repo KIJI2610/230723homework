7f944491e75c9fc942ff99ee1effa8b3f95139b2
Alexey <kalnitskiy2018@mail.ru>
2023-07-23 14:02:01 +0500

    ex3

---
+++ ex3/Program.cs
@@ -0,0 +1,27 @@
+﻿double[] arr = new double[5];
+for (int i = 0; i < arr.Length; i++)
+{   
+    double a = new Random().NextDouble()*(10.0 - 1.0)*1.0;
+    arr[i] = a;
+    string astr = arr[i].ToString("F2");
+    System.Console.WriteLine(astr);
+}
+double max = 0;
+double min = 0;
+for (int i = 1; i < arr.Length; i++)
+{
+   if(arr[i] > arr[i-1]){
+    max = arr[i];
+   }
+}
+System.Console.WriteLine("MAX: "+max.ToString("F2"));
+
+for (int i = 1; i < arr.Length; i++)
+{
+   if(arr[i] < arr[i-1]){
+    min = arr[i];
+   }
+}
+System.Console.WriteLine("MIN: "+min.ToString("F2"));
+double raz = max-min;
+System.Console.WriteLine("Разница: "+ raz.ToString("F2"));
